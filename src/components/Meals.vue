79a857c07401b3a914f6b1657ac3a5e492217a72
1akai1 <2akai@mail.ru>
2023-04-27 03:55:49 +0600

    add base, workflows

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -1,26 +1,39 @@
 <script setup>
 import MealItem from './MealItem.vue'
 
-const {meals} = defineProps({
-  meals: {
-    required: true,
-    type: Object
-  }
+const { meals } = defineProps({
+	meals: {
+		required: true,
+		type: Object,
+	},
 })
 </script>
 
 <template>
-	<div v-if="meals.length" class="grid md:grid-cols-3 grid-cols-1 p-8 gap-5 max-w-[1100px] m-auto">
+	<div
+		v-if="meals.length"
+		class="
+			grid
+			md:grid-cols-3
+			grid-cols-1
+			p-8
+			gap-5
+			max-w-[1100px]
+			m-auto">
 		<MealItem
 			v-for="meal of meals"
 			:key="meal.idMeal"
-			:meal="meal"/>
+			:meal="meal" />
 	</div>
-	<div v-else class="flex justify-center text-gray-600 mt-10">
+	<div
+		v-else
+		class="
+			flex
+			justify-center
+			mt-10
+			text-gray-600">
 		There are no meals
 	</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
